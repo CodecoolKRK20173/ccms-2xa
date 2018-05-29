2b7441ab2d566a3fe7e4dd868faa2c17fa3c154e
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-05-29 14:44:59 +0200

    Changes in packages

--- src/main/java/com/codecool/java/ccms_2xa/App.java
+++ src/main/java/com/codecool/java/ccms_2xa/App.java
@@ -1,6 +1,8 @@
 package com.codecool.java.ccms_2xa;
 
-import com.codecool.java.ccms_2xa.model.*;
+import com.codecool.java.ccms_2xa.model.lists.*;
+import com.codecool.java.ccms_2xa.model.records.*;
+
 
 /**
  * Hello world!
@@ -11,7 +13,7 @@ public class App
     public static void main( String[] args )
     {
 	User jerzy = new Manager("Jerzy", "1234", "a@i.pl", "JerzyManager");
-        User piotr = new Mentor("Piotr", "1234", "p@k.pl", "PiotrMentor");
-        System.out.println( "Hello World!" );
+    User piotr = new Mentor("Piotr", "1234", "p@k.pl", "PiotrMentor");
+    System.out.println( "Hello World!" );
     }
 }
--- src/main/java/com/codecool/java/ccms_2xa/Main.java
+++ src/main/java/com/codecool/java/ccms_2xa/Main.java
@@ -1,5 +1,8 @@
 package com.codecool.java.ccms_2xa.model;
 
+import com.codecool.java.ccms_2xa.model.editors.*;
+import com.codecool.java.ccms_2xa.model.records.*;
+
 public class Main{
     public static void main (String args[]) {
         //User admin = new User("Admin", "1234", "a@i.pl", "Ania");
