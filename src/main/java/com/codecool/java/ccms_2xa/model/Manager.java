5b90bb9c34d312a59297a888bd7daa101ed4be12
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-05-29 14:47:08 +0200

    Delete unneeded files

--- src/main/java/com/codecool/java/ccms_2xa/model/Manager.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.java.ccms_2xa.model;
-
-public class Manager extends User {
-    public Manager (String login, String password, String mail, String name) {
-        super(login, password, mail, name);
-    }
-
-}
--- src/main/java/com/codecool/java/ccms_2xa/model/Mentor.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.java.ccms_2xa.model;
-
-public class Mentor extends User {
-    public Mentor (String login, String password, String mail, String name) {
-        super(login, password, mail, name);
-    }
-}
